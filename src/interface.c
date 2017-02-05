98270100cd02988fcd77fc552271ab1f2c3f09da
Charles Reiss <woggle@apache.org>
2017-02-05 15:15:35 -0500

    compile options flag

--- src/interface.c
+++ src/interface.c
@@ -193,6 +193,7 @@ show_program_usage(char *prog)
     fprintf(stderr, "        -nodetach        do not detach server process\n");
     fprintf(stderr, "        -resolver PATH   path to fb-resolver program\n");
     fprintf(stderr, "        -version         display this server's version.\n");
+    fprintf(stderr, "        -compileoptions  display this server's compilation options.\n");
     fprintf(stderr, "        -help            display this message.\n");
 #ifdef WIN32
     fprintf(stderr,
@@ -4216,6 +4217,9 @@ main(int argc, char **argv)
 	    } else if (!strcmp(argv[i], "-version")) {
 		printf("%s\n", VERSION);
 		exit(0);
+            } else if (!strcmp(argv[i], "-compileoptions")) {
+                printf("%s\n", compile_options);
+                exit(0);
 	    } else if (!strcmp(argv[i], "-dbin")) {
 		if (i + 1 >= argc) {
 		    show_program_usage(*argv);
