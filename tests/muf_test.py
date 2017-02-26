f54148f13b6feb418734a599bc6c44128f958b57
Charles Reiss <woggle@apache.org>
2017-02-26 16:11:42 -0500

    array tests

--- tests/muf_test.py
+++ tests/muf_test.py
@@ -64,6 +64,9 @@ i
   dup 0 array_getitem 1 = 
   over 1 array_getitem "two" strcmp not and
   over 2 array_getitem 3.0 = and
+  over array_count 6 = and
+  over array_last swap 5 = and and
+  over array_first swap 0 = and and
   if
     me @ "Test passed." notify
   else
@@ -90,10 +93,14 @@ i
   { 1 "two" 3.0 4 5 6 }list
   "new two" swap 1 array_setitem
   "new value" swap array_appenditem
+  3 array_delitem
   dup 0 array_getitem 1 = 
   over 1 array_getitem "new two" strcmp not and
   over 2 array_getitem 3.0 = and
-  over 6 array_getitem "new value" strcmp not and
+  over 3 array_getitem 5 = and
+  over 5 array_getitem "new value" strcmp not and
+  over array_count 6 = and
+  over array_last swap 5 = and and
   if
     me @ "Test passed." notify
   else
