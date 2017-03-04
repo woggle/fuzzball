ad93bf07990327ed4cdef94162a9e484cd907c8a
Charles Reiss <woggle@apache.org>
2017-03-04 13:20:35 -0500

    +p_error_test.py

---
+++ tests/p_error_test.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3.6
+import asyncio
+import logging
+import re
+import unittest
+
+from fbmuck.server import Server, ServerTestBase, MufProgramTestBase
+
+class TestSetClearError(MufProgramTestBase):
+    def test_set_clear(self):
+        result = self._test_program(b"""
+: main
+    error? if "error initially set" abort then
+    "DIV_ZERO" set_error not if "failed set DIV_ZERO" abort then
+    error? not if "error? false" abort then
+    "NAN" set_error not if "failed set NAN" abort then
+    "IBOUNDS" set_error not if "failed set IBOUNDS" abort then
+    "DIV_ZERO" is_set? not if "DIV_ZERO unset" abort then
+    "IBOUNDS" is_set? not if "IBOUNDS unset" abort then
+    err_divzero? not if "err_divzero? false" abort then
+    err_ibounds? not if "err_ibounds? false" abort then
+    0 error_name "DIV_ZERO" strcmp if "0 error_name not DIV_ZERO" abort then
+    0 error_str "division by zero" instring not if "0 error_str does not contain division by zero" abort then
+    "DIV_ZERO" error_bit 0 = not if "DIV_ZERO error_bit is not 0" abort then
+    clear
+    error? if "error? true" abort then
+    err_divzero? if "err_divzero? true" abort then
+    err_ibounds? if "err_ibounds? true" abort then
+    me @ "Test passed." notify
+;
+""")
+    
+    def test_error_num(self):
+        result = self._test_program(b"""
+: main
+    error_num 5 = if
+        me @ "Test passed." notify
+    then
+;
+""")
+    
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    unittest.main()
