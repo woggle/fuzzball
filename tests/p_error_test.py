d58d5e752977e6a4a0c4df80003a10bcdfa59eab
Charles Reiss <woggle@apache.org>
2017-03-04 13:26:22 -0500

    add clear_error tests

--- tests/p_error_test.py
+++ tests/p_error_test.py
@@ -13,15 +13,20 @@ class TestSetClearError(MufProgramTestBase):
     error? if "error initially set" abort then
     "DIV_ZERO" set_error not if "failed set DIV_ZERO" abort then
     error? not if "error? false" abort then
-    "NAN" set_error not if "failed set NAN" abort then
     "IBOUNDS" set_error not if "failed set IBOUNDS" abort then
     "DIV_ZERO" is_set? not if "DIV_ZERO unset" abort then
     "IBOUNDS" is_set? not if "IBOUNDS unset" abort then
     err_divzero? not if "err_divzero? false" abort then
     err_ibounds? not if "err_ibounds? false" abort then
+    "IBOUNDS" clear_error
+    err_ibounds? if "err_ibounds? true" abort then
+    0 clear_error
+    err_divzero? if "err_divzero? true" abort then
+    error? if "error? true" abort then
     0 error_name "DIV_ZERO" strcmp if "0 error_name not DIV_ZERO" abort then
     0 error_str "division by zero" instring not if "0 error_str does not contain division by zero" abort then
     "DIV_ZERO" error_bit 0 = not if "DIV_ZERO error_bit is not 0" abort then
+    "DIV_ZERO" set_error "IBOUNDS" set_error
     clear
     error? if "error? true" abort then
     err_divzero? if "err_divzero? true" abort then
