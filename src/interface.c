2d1a27cf39b0befdc99acb31a9e22dc8bcad662e
Charles Reiss <woggle@apache.org>
2017-04-23 15:24:25 -0400

    fuzzing: limit number of cycles

--- src/interface.c
+++ src/interface.c
@@ -4636,7 +4636,8 @@ main(int argc, char **argv)
             free_prog_text(lines);
 	    PROGRAM_SET_FIRST(program, NULL);
             add_muf_delayq_event(0, -1, 1, 0, -1, program, "", "", 0);
-	    while (next_muckevent_time() == 0) {
+            int count = 0;
+	    while (next_muckevent_time() == 0 && ++count < 100) {
 		next_muckevent();
 		muf_event_process();
 	    }
